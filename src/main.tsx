b78f62b8f18b432ba9a2d919e763ec6f7218cb1d
davenarchives <daventheabyss@gmail.com>
2026-01-19 00:24:34 +0800

    refactor: update main entry point with providers

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import './index.css'
 import App from './App.tsx'
 
 import { AnimeProvider } from './context/AnimeContext';
+import { AuthProvider } from './context/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AnimeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AnimeProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <AnimeProvider>
+          <App />
+        </AnimeProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
